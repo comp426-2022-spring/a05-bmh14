ed4477258b7e9f03ab480a85e447694aa8e451c0
Bodhi Harmony <69818328+bmh14@users.noreply.github.com>
2022-04-29 15:50:30 -0400

    additions

--- index.js
+++ index.js
@@ -1,20 +1,76 @@
-// Place your server entry point code here
-// Require Express.js and minimist
-const express = require("express")
-const args = require('minimist')(process.argv.slice(2));
-const port = args["port"] || args.p || 5000
-const app = express()
-console.log(args)
-
-// Require db
-const db = require('./database.js')
+// Require minimist module (make sure you install this one via npm).
+// Require minimist module
+const args = require('minimist')(process.argv.slice(2))
+// See what is stored in the object produced by minimist
+//console.log('Command line arguments: ', args)
+// Store help text 
+const help = (`
+server.js [options]
+--port, -p	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+--debug, -d If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+--help, -h	Return this message and exit.
+`)
+// If --help, echo help text and exit
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
+}
+// Define app using express
+var express = require('express')
+var app = express()
 // Require fs
-const fs = require('fs');
+const fs = require('fs')
 // Require morgan
 const morgan = require('morgan')
-
-app.use(express.urlencoded({ extended: true }));
+// Require database SCRIPT file
+const logdb = require('./src/services/database.js')
+// Make Express use its own built-in body parser
+// Allow urlencoded body messages
+//app.use(express.urlencoded({ extended: true }));
+// Allow json body messages
 app.use(express.json());
+// Server port
+const port = args.port || args.p || process.env.PORT || 5000
+// If --log=false then do not create a log file
+if (args.log == 'false') {
+    console.log("NOTICE: not creating file access.log")
+} else {
+// Use morgan for logging to files
+    const logdir = './log/';
+
+    if (!fs.existsSync(logdir)){
+        fs.mkdirSync(logdir);
+    }
+// Create a write stream to append to an access.log file
+    const accessLog = fs.createWriteStream( logdir+'access.log', { flags: 'a' })
+// Set up the access logging middleware
+    app.use(morgan('combined', { stream: accessLog }))
+}
+// Always log to database
+app.use((req, res, next) => {
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referrer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    };
+    const stmt = logdb.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
+    //console.log(info)
+    next();
+})
 
 // functions
 function coinFlip() {
@@ -54,129 +110,70 @@ function flipACoin(call) {
     }
     return {"call": call, "flip": flip, "result": result};
 }
-// Start an app server
-const server = app.listen(port, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',port))
+
+// Serve static HTML public directory
+app.use(express.static('./public'))
+
+// READ (HTTP method GET) at root endpoint /app/
+app.get("/app/", (req, res, next) => {
+    res.json({"message":"Your API works! (200)"});
+	res.status(200);
 });
 
-app.get('/app/', (req, res, next) => { // checkpoint
-  res.json({"message":"working API(200)"});
-  res.status(200);
+// Endpoint /app/flip/ that returns JSON {"flip":"heads"} or {"flip":"tails"} 
+// corresponding to the results of the random coin flip.
+app.get('/app/flip/', (req, res) => {
+    const flip = coinFlip()
+    res.status(200).json({ "flip" : flip })
 });
 
-if (args.log == 'false') {
-  console.log("Not creating file access.log")
-} else {
-  const accessLog = fs.createWriteStream('access.log', { flags: 'a'})
-  app.use(morgan('combined', {stream: accessLog }))
-}
-
-// Help text
-const help = (`
-server.js [options]
---port, -p	Set the port number for the server to listen on. Must be an integer
-            between 1 and 65535.
---debug, -d If set to true, creates endlpoints /app/log/access/ which returns
-            a JSON access log from the database and /app/error which throws 
-            an error with the message "Error test successful." Defaults to 
-            false.
---log		If set to false, no log files are written. Defaults to true.
-            Logs are always written to database.
---help, -h	Return this message and exit.
-`)
-
-// If --help
-if (args.help || args.h) {
-  console.log(help)
-  process.exit(0)
-}
-
-// Middleware
-app.use((req, res, next) => {
-  let logData = {
-      remoteaddr: req.ip,
-      remoteuser: req.user,
-      time: Date.now(),
-      method: req.method,
-      url: req.url,
-      protocol: req.protocol,
-      httpversion: req.httpVersion,
-      status: res.statusCode,
-      referrer: req.headers['referer'],
-      useragent: req.headers['user-agent']
-  };
-  console.log(logData)
-  const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-  const info = stmt.run(logData.remoteaddr, logData.remoteuser, logData.time, logData.method, logData.url, logData.protocol, logData.httpversion, logData.status, logData.referrer, logData.useragent)
-  next();
+app.post('/app/flip/coins/', (req, res, next) => {
+    const flips = coinFlips(req.body.number)
+    const count = countFlips(flips)
+    res.status(200).json({"raw":flips,"summary":count})
 })
 
-// Endpoints if --debug is true
-if(args.debug === true) {
-  // /app/log/access endpoint
-  app.get('/app/log/access/', (req, res) => {
-    const stmt = db.prepare("SELECT * FROM accesslog").all()
-    res.status(200).json(stmt)
-  });
-
-  // /app/log/access endpoint
-  app.get('/app/error', (req, res) => {
-    throw new Error('Error, test successful')
-  });
-}
-
-// checkpoints and endpoints
-app.get('/app/', (req, res) => { 
-  // Respond with status 200
-      res.statusCode = 200;
-  // Respond with status message "OK"
-      res.statusMessage = 'OK';
-      res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-      res.end(res.statusCode+ ' ' +res.statusMessage)
+app.get('/app/flips/:number', (req, res, next) => {
+    const flips = coinFlips(req.params.number)
+    const count = countFlips(flips)
+    res.status(200).json({"raw":flips,"summary":count})
 });
 
-// Flip one coin endpoint
-app.get('/app/flip/', (req, res) => {
-      res.statusCode = 200;
-      res.statusMessage = 'OK';
-      // Flip coin
-      const result = coinFlip();
-      // Result
-      res.json({"flip": result});
-  });
+app.post('/app/flip/call/', (req, res, next) => {
+    const game = flipACoin(req.body.guess)
+    res.status(200).json(game)
+})
 
-// Flip multiple
-app.get('/app/flips/:number', (req, res) => {
-  // Status code and message
-      res.statusCode = 200;
-      res.statusMessage = 'OK';
-      // Flip multiple coins
-      const flips = coinFlips(req.params.number);
-      // Result
-      res.json({"raw": flips, "summary": countFlips(flips)});
-  });
+app.get('/app/flip/call/:guess(heads|tails)/', (req, res, next) => {
+    const game = flipACoin(req.params.guess)
+    res.status(200).json(game)
+})
 
-// Call heads
-app.get('/app/flip/call/heads', (req, res) => {
-  // Respond with status and message
-      res.statusCode = 200;
-      res.statusMessage = 'OK';
-      // Flip coin and return result as given by function
-      const result = flipACoin("heads");
-      res.json(result);
-  });
+if (args.debug || args.d) {
+    app.get('/app/log/access/', (req, res, next) => {
+        const stmt = logdb.prepare("SELECT * FROM accesslog").all();
+	    res.status(200).json(stmt);
+    })
 
-// Call tails
-app.get('/app/flip/call/tails', (req, res) => {
-  // Respond with status and message
-      res.statusCode = 200;
-      res.statusMessage = 'OK';
-      // Flip coin and return result as given by function
-      const result = flipACoin("tails");
-      res.json(result);
-  });
+    app.get('/app/error/', (req, res, next) => {
+        throw new Error('Error test works.')
+    })
+}
 
-  // Default response for any other request
+// Default API endpoint that returns 404 Not found for any endpoints that are not defined.
 app.use(function(req, res){
-  res.status(404).send('404 NOT FOUND')
+    const statusCode = 404
+    const statusMessage = 'NOT FOUND'
+    res.status(statusCode).end(statusCode+ ' ' +statusMessage)
+});
+
+// Start server
+const server = app.listen(port, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",port))
+});
+// Tell STDOUT that the server is stopped
+process.on('SIGINT', () => {
+    server.close(() => {
+		console.log('\nApp stopped.');
+	});
 });
--- public/main.js
+++ public/main.js
@@ -1,8 +1,81 @@
 // Focus div based on nav button click
 
 // Flip one coin and show coin image to match result when button clicked
+const coin = document.getElementById("coin")
+// Add event listener for coin button in div#single
+coin.addEventListener("click", flipCoin)
+// Asynchronus function that listens for response
+async function flipCoin() {
+    // Endpoint URL
+    const endpoint = "app/flip/"
+    const url = document.baseURI+endpoint
+    // Send request to API and wait for response
+    await fetch(url)
+    // Receive response 
+    .then(function(response) {
+        return response.json();
+        })
+        // Process response
+        .then(function(result) {
+            console.log(result);
+            document.getElementById("result").innerHTML = result.flip;
+            document.getElementById("quarter").setAttribute("src", "assets/img/"+result.flip+".png");
+          });
 
 // Flip multiple coins and show coin images in table as well as summary results
-// Enter number and press button to activate coin flip series
+// Enter number and press button to activate coin flip series\
+const coins = document.getElementById("coins")
+// Add listener
+coins.addEventListener("submit", flipCoins)
+// Asynchronus listening function for multiple coins
+async function flipCoins(event) {
+    // Prevent the default browser event 
+    event.preventDefault();
+    // Endpoint URL
+    const endpoint = "app/flip/coins/"
+	const url = document.baseURI+endpoint
+    // Extract data from form
+    const formEvent = event.currentTarget
+    // Put data in FormData in order to wait for either a response or an error to be thrown
+    try {
+		const formData = new FormData(formEvent);
+        const flips = await sendFlips({ url, formData });
+        console.log(flips);
+        document.getElementById("heads").innerHTML = "Heads: "+flips.summary.heads;
+		document.getElementById("tails").innerHTML = "Tails: "+flips.summary.tails;
+        document.getElementById("coinlist").innerHTML = coinList(flips.raw);
+	} catch (error) {
+		console.log(error);
+	}
+}
 
 // Guess a flip by clicking either heads or tails button
+// Input value to be sent to API
+const call = document.getElementById("call")
+// Add listener
+call.addEventListener("submit", flipCall)
+// Asychronus function for guess
+async function flipCall(event) {
+    // Prevent default browser event
+    event.preventDefault();
+    // Endpoint URL
+    const endpoint = "app/flip/call/"
+	const url = document.baseURI+endpoint
+    // Extract data
+    const formEvent = event.currentTarget
+    // Put data in FormData in order to wait for either a response or an error to be thrown
+    try {
+		const formData = new FormData(formEvent); 
+        const results = await sendFlips({ url, formData });
+        console.log(results);
+        document.getElementById("choice").innerHTML = "Guess: "+results.call;
+		document.getElementById("actual").innerHTML = "Actual: "+results.flip;
+		document.getElementById("results").innerHTML = "Result: "+results.result;
+        document.getElementById("coingame").innerHTML = '<li><img src="assets/img/'+results.call+'.png" class="bigcoin" id="callcoin"></li><li><img src="assets/img/'+results.flip+'.png" class="bigcoin"></li><li><img src="assets/img/'+results.result+'.png" class="bigcoin"></li>';
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+
+}
